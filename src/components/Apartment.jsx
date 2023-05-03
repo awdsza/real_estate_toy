29a93b85ab0954eabd6f157fdc3b7f169c835e23
awdsza <37355034+awdsza@users.noreply.github.com>
2023-05-04 01:58:44 +0900

    [feat]아파트 목록 컴포넌트 구현

--- src/components/Apartment.jsx
+++ src/components/Apartment.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Apartment() {
-  return <div>아파트</div>;
+export default function Apartment({ data }) {
+  const { as1, as2, as3, bjdCode, kaptName } = data;
+  return (
+    <Link to={`/apartment/${bjdCode}`}>
+      <li
+        className={`py-5 box-content bg-[url('./images/bgAPT.jpeg')] bg-cover bg-no-repeat w-full`}
+      >
+        <dl>
+          <dt className="text-base font-semibold">{kaptName}</dt>
+          <dd className="text-xs">{`${as1} ${as2} ${as3}`}</dd>
+        </dl>
+      </li>
+    </Link>
+  );
 }
--- src/components/Apartments.jsx
+++ src/components/Apartments.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Apartment from "./Apartment";
+import EstateDataAPI from "../api/EstateDataAPI";
 export default function Apartments() {
+  const api = new EstateDataAPI();
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    async function getApartList() {
+      setList(await api.getApartList());
+    }
+    getApartList();
+  }, []);
   return (
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+    <ul className="grid grid-cols-1 gap-1 gap-y-5 mt-5">
+      {list.map((l) => (
+        <Apartment data={l} key={l.kaptCode} />
+      ))}
     </ul>
   );
 }
