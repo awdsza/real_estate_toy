a9128633cae0830eb269912472187422d2d13cc4
awdsza <37355034+awdsza@users.noreply.github.com>
2023-05-04 20:59:40 +0900

    [feat]리스트 컴포넌트 구현.

--- src/components/Apartment.jsx
+++ src/components/Apartment.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Apartment({ data }) {
-  const { as1, as2, as3, bjdCode, kaptName } = data;
+  const { as1, as2, as3, bjdCode, kaptName, roadName1, roadName2 } = data;
   return (
     <Link to={`/apartment/${bjdCode}`}>
       <li
         className={`py-5 box-content bg-[url('./images/bgAPT.jpeg')] bg-cover bg-no-repeat w-full`}
       >
         <dl>
-          <dt className="text-base font-semibold">{kaptName}</dt>
-          <dd className="text-xs">{`${as1} ${as2} ${as3}`}</dd>
+          <dt className="text-base text-baseColor font-semibold mb-5">
+            {kaptName}
+          </dt>
+          <dd className="text-xs font-medium mb-2">{`${as1} ${as2} ${as3}`}</dd>
+          <dd className="text-xs font-medium mb-2">{roadName1}</dd>
+          <dd className="text-xs font-medium">{roadName2}</dd>
         </dl>
       </li>
     </Link>
--- src/components/Apartments.jsx
+++ src/components/Apartments.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Apartment from "./Apartment";
-import EstateDataAPI from "../api/EstateDataAPI";
+const sampleData = [
+  {
+    as1: "서울시",
+    as2: "은평구",
+    as3: "쌍문동",
+    bjdCode: "1111100111",
+    kaptName: "쌍문 삼성 레미안",
+    roadName1: "서울시 은평구 쌍문로100",
+    roadName2: "서울시 은평구 쌍문리 33-1",
+  },
+];
 export default function Apartments() {
-  const api = new EstateDataAPI();
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    async function getApartList() {
-      setList(await api.getApartList());
-    }
-    getApartList();
-  }, []);
+  const [list, setList] = useState(sampleData);
   return (
-    <ul className="grid grid-cols-1 gap-1 gap-y-5 mt-5">
+    <ul className="grid grid-cols-1 gap-1 gap-y-4 mt-5">
       {list.map((l) => (
         <Apartment data={l} key={l.kaptCode} />
       ))}
