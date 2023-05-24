d446ccab0858b6b773b3ed28bee305491dfffb83
awdsza <37355034+awdsza@users.noreply.github.com>
2023-05-25 01:48:32 +0900

    [feat]법정동 코드검색 reactQuery적용

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import { JusoProvider } from "./context/JusoProvider";
 import { HeaderProvider } from "./context/HeaderProvider";
 import { EstateAPIProvider } from "./context/EstateAPIProvider";
 import { CommonProvider } from "./context/CommonProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 function App() {
   return (
     <section className="my-3 mx-2 relative">
-      <CommonProvider>
-        <HeaderProvider>
-          <JusoProvider>
-            <SearchHeader />
-          </JusoProvider>
-        </HeaderProvider>
-        <EstateAPIProvider>
-          <div id="detail">
-            <Outlet />
-          </div>
-        </EstateAPIProvider>
-      </CommonProvider>
+      <QueryClientProvider client={queryClient}>
+        <CommonProvider>
+          <HeaderProvider>
+            <JusoProvider>
+              <SearchHeader />
+            </JusoProvider>
+          </HeaderProvider>
+          <EstateAPIProvider>
+            <div id="detail">
+              <Outlet />
+            </div>
+          </EstateAPIProvider>
+        </CommonProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </section>
   );
 }
